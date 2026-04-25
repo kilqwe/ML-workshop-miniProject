2b14cbd6b646c89bcc43686dc13342e21dbe49f4
kilqwe <shreyas0514@gmail.com>
2026-04-25 15:14:37 +0530

    CORS fix 1

--- backend/app/main.py
+++ backend/app/main.py
@@ -24,7 +24,11 @@ app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.ALLOWED_ORIGINS,
+    allow_origins=[
+        "http://localhost:3000",
+        "https://ml-workshop-mini-project.vercel.app",
+        "https://ml-workshop-mini-project.vercel.app/"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
